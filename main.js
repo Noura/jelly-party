f9ac0f4bc6e1f448e6ec224290145481afbeb84b
Noura <culurienneldoreth@gmail.com>
2016-01-17 18:41:27 -0800

    cut out the audio. simplified as much as possible. now it is just dots that move around.

--- main.js
+++ main.js
@@ -1,243 +1,217 @@
-var context;
-var col;
-var _x, _y;
-var synths = {};
-var pressed;
-var time=(new Date()).getTime();
-var client_id;
+// everyone at the party, including ourselves
+var visitors = {};
 
-var THROTTLE_MS = 50,  // only emit on socket once per this many ms
+// our ID. our data is saved in visitors[myID]
+// ( other visitors have their own ID, and their data is saved in visitors too )
+var myID;
 
-    // the smaller the coeff the more the ball lags behind the mouse. the higher
-    // it is the jumpier throttled move messages will appear.
-    SMOOTHING_COEFFICIENT = 0.3; // valid values: (0-1]
-
-// updates synth location, pitch & filter frequency on every animation
-// frame. this is the only place that synth.coords is written to.
-function updateSynths(){
-    window.requestAnimationFrame(updateSynths);
-    for (var cid in synths){
-        var synth = synths[cid],
-            coef = SMOOTHING_COEFFICIENT,
-            synthArgs;
-
-        if(!synth.started){
-            synth.started = true;
-        }
-
-        // if coords are null, set them to synth.dest. this prevents the synth
-        // blob from sliding to a note on mouse down. it just instantly appears
-        // there.
-        synth.coords.x = synth.coords.x == null ? synth.dest.x : synth.coords.x;
-        synth.coords.y = synth.coords.y == null ? synth.dest.y : synth.coords.y;
-
-        // simple exponential smoothing
-        synth.coords.x = synth.dest.x * coef + (1 - coef) * synth.coords.x;
-        synth.coords.y = synth.dest.y * coef + (1 - coef) * synth.coords.y;
-
-        synthArgs = synthmap(synth.coords.x, synth.coords.y);
-
-        $('#synth_'+cid).css({
-            'left' : (synth.coords.x*$(window).width()-20) + 'px',
-            'top' : synth.coords.y*$(window).height()-20 + 'px',
-            'background-color': synth.color,
-            'opacity' : '0.7'
-        });
-
-    }
-}
+//////////////////////////////////////////////////////////////////
+// LET'S GET THIS PARTY STARTED //////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
+    // where the party's at. where we will draw everyone
+    var $main_content = document.getElementById("main-content");
 
-  var color = hsvToRgb(Math.random(),1,1);
-  col = 'rgb(' + color.join(',') + ')';
-
-    $fun = document.getElementById("fun");
-    hammertime = Hammer($fun, {
-      prevent_default: true,
-      no_mouseevents: true
+    // hammertime helps us deal with mouse clicks and touch screen events
+    hammertime = Hammer($main_content, {
+        prevent_default: true,
+        no_mouseevents: true
     })
     .on('touch', function(event){
-      touchActivate(event);
+        touchActivate(event);
     })
     .on('drag', function(event){
-      touchActivate(event);
+        touchActivate(event);
     })
     .on('release', function(event){
-      touchDeactivate();
+        touchDeactivate();
     });
-    updateSynths();
+
+    drawVisitors();
 });
 
-// Returns a version of `fun` that will only run once very `ms` milliseconds, no
-// matter how often it's called. Function invocations f1, f2, f3, f4 all
-// occuring within a timespan less than `ms`, will result in f1 and f4 being
-// called, at time 0 and time `ms`, respectively.
-function throttle(ms, fun){
-    var then = performance.now(),
-        that = this,
-        lastArgs = null,
-        timeout = null;
 
-    return function(){
-        var now = performance.now(),
-            args = Array.prototype.slice.apply(arguments);
+//////////////////////////////////////////////////////////////////
+// DRAWING EVERYONE //////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
-        if(timeout) window.clearTimeout(timeout);
+function drawVisitors(){
+    for (var ID in visitors) {
+        var visitor = visitors[ID];
 
-        if(now - then > ms){
-            fun.apply(that, args);
-            then = now;
-        } else {
-            // if it hasn't been long enough, schedule the most recent function
-            // call to fire after `ms` have passed since the last successful
-            // function call.
-            lastArgs = args;
-            timeout = window.setTimeout(function(){
-                fun.apply(that, args);
-            }, ms - (now - then));
+        // if this visitor doesn't have an HTML element yet, make one for them
+        if ($('#visitor_'+ID).length === 0) {
+            $('#main-content').append('<span class="visitor" id="visitor_'+ID+'"></span>');
         }
-    };
+
+        // update the visitor's CSS
+        $('#visitor_'+ID).css({
+            'left' : visitor.x*$(window).width()-20 + 'px',
+            'top' : visitor.y*$(window).height()-20 + 'px',
+            'background-color': visitor.color,
+            'opacity' : '0.7'
+        });
+    }
+
+    // keep asking the window to draw again and again
+    window.requestAnimationFrame(drawVisitors);
 }
 
+//////////////////////////////////////////////////////////////////
+// MOVING AROUND /////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
+// whether we are clicking or touching the screen
+var pressed;
+
+// update our location based on mouse clicks or touchscreen events
 var touchActivate = function(event){
-  pressed= true;
-  var c = event.gesture.center;
-  var x = c.pageX;
-  var y = c.pageY;
-  $("#press").html("");
-  var xRatio = x/$(window).width();
-  var yRatio = y/$(window).height();
-  var data = {
-    x:xRatio,
-    y:yRatio,
-    col:col,
-    id:client_id,
-  }
-  if(typeof client_id != "undefined"){
-    socket.emit('move', data);
-  }
-  playSynth(data);
+    // the event variable gives us information about the mouse click or touch
+    // screen event. mostly we care about where we are clicking or touching the
+    // screen because that's how we tell our avatar to move there.
+
+    // if we don't have an ID yet, we aren't allowed to do anything.
+    // ( we get an ID once we connect with the server )
+    if (myID === undefined) {
+        return; // this exits this function
+    }
+
+    console.log('touchActivate');
+
+    pressed = true;
+
+    // where the click or touch is happening
+    var c = event.gesture.center;
+    var x = c.pageX;
+    var y = c.pageY;
+
+    // ratios, so other people can draw our location on multiple screen sizes
+    // so like instead of saying "we are 200px to the right", we'd say "we are
+    // halfway across the screen", so people can know where to draw us on THEIR
+    // screen even if their screen is a different size than ours
+    var xRatio = x/$(window).width();
+    var yRatio = y/$(window).height();
+    var data = {
+        x: xRatio,
+        y: yRatio,
+        color: visitors[myID].color
+    };
+    updateVisitor(myID, data);
+
+    // share our location with everyone else at the party, so they can see us
+    // on their screen too!
+    share_my_data();
 }
 
 var touchDeactivate = function(){
-  pressed=false;
+    console.log('touchDeactivate');
+    pressed = false;
 }
 
-//
-//socketio
+var updateVisitor = function(ID, data){
+    console.log('updateVisitor');
 
-var socket = io.connect('http://'+window.location.hostname);
-socket.emit = throttle.call(socket, THROTTLE_MS, socket.emit);
+    if (visitors[ID] === undefined) {
+        visitors[ID] = {};
+    }
 
-function synthmap(x,y){
-  tx = 40*Math.pow(2,x*5);
-  ty = 100*Math.pow(2,(1-y)*6);
-  return [tx,ty]
-}
-
-function map_range(value, low1, high1, low2, high2) {
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
-
-
-socket.on('connect',function(){
-  client_id = socket.socket.sessionid;
-  console.log(client_id);
-
-  if(typeof client_id != "undefined"){
-  $('body').append('<span class="synth" id="synth_'+client_id+'"><span style="display:none;" class="chat"/></span>');
-  }
-
-  $('#synth_'+client_id).css({
-    'left' : ($(window).width()/2-20) + 'px',
-    'top' : $(window).height()/2-20 + 'px',
-    'background-color': col,
-    'opacity' : '0.2'
-  });
-
-  if(typeof client_id !="undefined"){
-    synths[client_id]=prepSynths();
-  }
-
-});
-
-socket.on('move', function (data) {
-  playSynth(data);
-});
-
-var playSynth = function(data){
-  if($('#synth_'+data['id']).length == 0 && typeof data['id'] !="undefined") {
-    $('body').append('<span class="synth" id="synth_'+data['id']+'"><span style="display:none;" class="chat"/></span>');
-  }
-
-  if(!synths[data.id] && typeof data.id != "undefined"){
-    synths[data.id]=prepSynths();
-  }
-
-  var synth = synths[data.id];
-  if(synth){
-      synth.dest.x = data.x;
-      synth.dest.y = data.y;
-      synth.color = data.col;
-  }
+    visitors[ID].x = data.x;
+    visitors[ID].y = data.y;
+    visitors[ID].color = data.color;
 };
 
-socket.on('close', function (id) {
-  console.log('disconnect ' + id);
-  if(id in synths){
-    if(synths[id].started){
-    }
-  }
-  $('#synth_'+id).remove();
+//////////////////////////////////////////////////////////////////
+// COMMUNICATING WITH OTHER PARTIERS /////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+// socket is the way we communicate with the server. everyone else at the party
+// is also talking to the same server, so that's how we communicate with other
+// people at the party.
+var socket = io.connect('http://'+window.location.hostname);
+
+socket.on('connect',function(){
+    console.log('socket connect');
+
+    myID = socket.socket.sessionid;
+    console.log(myID);
+
+    // we add ourselves to the list of visitors
+    visitors[myID] = {};
+    visitors[myID].color = get_random_bright_color();
 });
 
+// this tells us that one of the other people has moved, so we want to update
+// their location
+socket.on('move', function (data) {
+    console.log('socket "move" message got data', data);
+    updateVisitor(data.ID, data);
+});
 
-function sendData(data) {
-  var now = (new Date()).getTime();
-  if(typeof client_id != "undefined" && pressed && (now-time>40)){
-    socket.emit('move', data);
-    time= (new Date()).getTime();
-  }
-  return true;
+socket.on('close', function (ID) {
+    console.log('socket close');
+    console.log('disconnect ' + ID);
+    $('#visitor_'+ID).remove();
+});
+
+//////////////////////////////////////////////////////////////////
+// HOW TO TELL EVERYONE ELSE WHERE WE ARE ////////////////////////
+// so they can draw us too ///////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+// the last point in time when we shared our data
+var time = (new Date()).getTime();
+function share_my_data() {
+    console.log('share_my_data');
+
+    var me = visitors[myID];
+    var my_data = {
+        x: me.x,
+        y: me.y,
+        color: me.color,
+        ID: myID
+    }
+
+    // we try to share out only when necessary, otherwise there would be too
+    // many messages and things would get jammed and laggy. so, we see what
+    // time it is now, and compare that to when we last shared out. we only
+    // share out at most every 40 milliseconds. also, we only share out when
+    // we are actively moving around (when "pressed" is true)
+    var now = (new Date()).getTime();
+    if ( pressed && (now-time>40) ) {
+        socket.emit('move', my_data);
+        time = (new Date()).getTime();
+    }
+    return true;
 }
 
+//////////////////////////////////////////////////////////////////
+// CHOOSING PRETTY COLORS ////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
-function prepSynths(){
-
-  return {
-      started: false,
-
-      // dest contains the destination coordinates the server gave us for this
-      // node. these are updated by use interaction / move messages from server.
-      dest: {x: 0, y: 0},
-
-      // coords contains the current on-screen location of the node. these
-      // coordinates transition smoothly between the coords in .dest
-      coords: {x: null, y: null},
-
-      color: null,
-  };
+function get_random_bright_color() {
+    var HSVcolor = hsvToRgb(Math.random(),1,1);
+    var RGBcolor = 'rgb(' + HSVcolor.join(',') + ')';
+    return RGBcolor;
 }
 
 function hsvToRgb(h, s, v){
-  var r, g, b;
+    var r, g, b;
 
-  var i = Math.floor(h * 6);
-  var f = h * 6 - i;
-  var p = v * (1 - s);
-  var q = v * (1 - f * s);
-  var t = v * (1 - (1 - f) * s);
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
 
-  switch(i % 6){
-    case 0: r = v, g = t, b = p; break;
-    case 1: r = q, g = v, b = p; break;
-    case 2: r = p, g = v, b = t; break;
-    case 3: r = p, g = q, b = v; break;
-    case 4: r = t, g = p, b = v; break;
-    case 5: r = v, g = p, b = q; break;
-  }
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
 
-  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
 }
